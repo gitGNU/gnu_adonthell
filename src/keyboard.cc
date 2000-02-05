ee9dbdb96d77fdf56d3d106c02c87aa0311e2cc7
Andrew Henderson <hendersa@db.erau.edu>
2000-02-05 16:02:15 +0000

    Keyboard codes put in keyboard.h

--- src/keyboard.cc
+++ src/keyboard.cc
@@ -4,13 +4,10 @@
 #include "types.h"
 #include "gfx.h"
 #include "keyboard.h"
-#ifdef SDL
 #include "SDL.h"
-#endif
 
 bool keyboard::charkeys[65536];
 
-#ifdef SDL
 u_int16 keyboard::last_key;
 
 int keyboard_init(void *) {
@@ -38,52 +35,6 @@ int keyboard_init(void *) {
 void keyboard::update_keyboard() {}
 u_int16 keyboard::getkeypressed() { return(last_key); }
 
-#else
-
-#include <ggi/ggi.h>
-
-void keyboard::update_keyboard()
-{
-  ggi_event event;
-  ggi_event_mask mask;
-  struct timeval t={0,0};
-  while((mask=ggiEventPoll(screen::vis,emKey,&t)))
-    {
-      ggiEventRead(screen::vis,&event,emKey);
-      if(event.any.type==evKeyPress)
-	{
-	  charkeys[event.key.sym]=1;
-	}else
-      if(event.any.type==evKeyRelease)
-	{
-	  charkeys[event.key.sym]=0;
-	}
-    }
-}
-
-u_int16 keyboard::getkeypressed()
-{
-  u_int16 c=0;
-  ggi_event event;
-  ggi_event_mask mask;
-  struct timeval t={0,0};
-  
-  if((mask=ggiEventPoll(screen::vis,emKey,&t)))
-    {
-      ggiEventRead(screen::vis,&event,emKey);
-      if(event.any.type==evKeyPress)
-	{
-	  c=event.key.sym;
-	  if(c==Shift_Key) charkeys[Shift_Key]=1;
-	}
-      if(event.any.type==evKeyRelease)
-	{
-	  charkeys[event.key.sym]=0;
-	}
-    }
-  return(c);
-}
-#endif
 
 bool keyboard::is_pushed(u_int16 key)
 {
