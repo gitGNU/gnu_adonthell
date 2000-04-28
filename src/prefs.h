5935b3300e6278623f157a9ed3171860bb2cb478
Kai Sterker <kai.sterker@gmail.com>
2000-04-28 12:13:21 +0000

    Scanner for  configuration file (.adonthellrc)

---
+++ src/prefs.h
@@ -0,0 +1,30 @@
+/*
+   $Id$
+
+   Copyright (C) 2000 Kai Sterker <kaisterker@linuxgames.com>
+   Part of the Adonthell Project http://adonthell.linuxgames.com
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+enum
+{
+    PREFS_UNKNOWN = 0,
+    PREFS_DEFAULT = 1,
+    PREFS_SECTION = 2,
+    PREFS_DATA = 3,
+    PREFS_END = 4,
+    PREFS_SCREEN_RESOLUTION = 5,
+    PREFS_SCREEN_MODE = 6,
+    PREFS_AUDIO_CHANNELS = 7,
+    PREFS_AUDIO_RESOLUTION = 8,
+    PREFS_AUDIO_SAMPLE_RATE = 9,
+    PREFS_AUDIO_VOLUME = 10,
+    PREFS_NUM = 11,
+    PREFS_STR = 12
+};
