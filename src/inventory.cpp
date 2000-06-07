3900bb77db054bcfb6fd7f5c0b1f9a41310e8dab
fremen <fremen>
2000-06-07 13:27:07 +0000

    The new inventory code I forgot before. Nothing here yet!

---
+++ src/inventory.cpp
@@ -0,0 +1,19 @@
+/*
+   $Id$
+
+   Copyright (C) 1999   The Adonthell Project
+   Part of the Adonthell Project http://adonthell.linuxgames.com
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY.
+
+   See the COPYING file for more details.
+*/
+
+#include "types.h"
+#include "mapitem.h"
+
+/* Will fill this in later ; dummy for now */
+
