908931e76cd0c27f5c773e014863cfea3586b8e3
Kai Sterker <kai.sterker@gmail.com>
2001-03-09 10:19:19 +0000

    Now also works when specifying a relative path to the script

--- src/tools/pydonthell/pydonthell.cpp
+++ src/tools/pydonthell/pydonthell.cpp
@@ -1,7 +1,7 @@
 /*
    $Id$
  
-   Copyright (C) 1999   The Adonthell Project
+   Copyright (C) 1999 - 2001 The Adonthell Project
    Part of the Adonthell Project http://adonthell.linuxgames.com
 
    This program is free software; you can redistribute it and/or modify
@@ -11,6 +11,8 @@
 
    See the COPYING file for more details.
 */
+
+#include <unistd.h>
 
 #include "game.h"
 #include "py_inc.h"
@@ -23,8 +25,18 @@ int main(int argc, char * argv[])
 {
     game the_game (0, NULL);
 
+    char *cwd = getcwd (NULL, 0);
+    char tmp[256];
+    
     // Init the game engine
     if (!the_game.init ()) 
         return 1;
+
+    if (argc > 1 && argv[1][0] != '/') 
+    {
+        sprintf (tmp, "%s/%s", cwd, argv[1]);
+        argv[1] = tmp;
+    }
+
     return Py_Main(argc,argv);
 }
