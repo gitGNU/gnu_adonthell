21941da4c5971a2a3409972528f53ba5f4cafa5a
Kai Sterker <kai.sterker@gmail.com>
2001-05-14 09:04:03 +0000

    FIXED: We have to remove the old comments *before* calculating the loop points. Everything packed into one function wasn't cool anymore, so I rewrote it completely :)

--- src/tools/oggloop/oggloop.c
+++ src/tools/oggloop/oggloop.c
@@ -35,176 +35,243 @@
 #include "vorbis/codec.h"
 #include "vcedit.h"
 
-int main (int argc, char *argv[]) {
-  OggVorbis_File ov;
-  vcedit_state *vc;
-  char tmp[32], file[512], changed = 0;
-  FILE *oggfile, *outfile;
-  double start = 0;
-  double end = 0;
-  int i, s, e, p_raw, p_pcm, size = 0;
+/* Write the changed OggVorbis comments */
+void write_file (char *file, vcedit_state *state)
+{
+    char tmpfile[512];
+    FILE *outfile;
 
-  /* open the file, our last argument */
-  oggfile = fopen (argv[argc-1], "r");
-
-  if (!oggfile) {
-    printf ("File \"%s\" not found.\n\n", argv[argc-1]);
-    exit (1);
-  }
-  
-  if (ov_open (oggfile, &ov, NULL, -1) < 0) {
-    printf ("Could not open input as an OggVorbis file.\n\n");
-    exit (1);
-  }
-
-  /* try to get start and end time */
-  for (i = 1; i < argc-1; i++) {
-    if (!strcmp ("-s", argv[i]) && i+1 < argc)
-      start = atof (argv[++i]);
-    if (!strcmp ("-e", argv[i]) && i+1 < argc)
-      end = atof (argv[++i]);
-  }
-  
-  /* print details about each logical bitstream in the input */
-  if (ov_seekable (&ov)) {
-    if (!end) end = ov_time_total (&ov, -1);
-
-    /* We want to loop a tune in ogg format the following way:
-
-              start           end
-       |------- + ------------ + -------|
-         intro        loop        outro
-
-       Get the starting point as pcm offset, since that makes the seeking
-       most precise */
-    ov_time_seek (&ov, start);
-    s = ov_pcm_tell (&ov);
-    printf ("\nStart (%gs):\t %i (pcm offset)\n", start, s);
-
-    /* To allow fast seeking, we'll get the start of the page containing
-       the pcm sample above, we then position the stream manually to the
-       page and skip data until we've found our pcm offset. That's nearly
-       as fast as ov_seek_raw () would be! */
-    ov_time_seek_page (&ov, start);
-    p_pcm = ov_pcm_tell (&ov);
-    p_raw = ov_raw_tell (&ov) - ov.oy.returned;
-
-    /* Get the end point as raw bytes */
-    ov_time_seek (&ov, end);
-    e = ov_raw_tell (&ov);
-    printf ("End (%gs):\t %i (raw bytes)\n", end, e);
-    fflush (stdout);      
-
-    /* close the file */
-    ov_clear (&ov);
-
-    /* open it for vcedit */
-    oggfile = fopen (argv[argc-1], "rb");
-
-    /* add stuff to the oggvorbis comment header */
-    vc = vcedit_new_state ();
-    if (vcedit_open (vc, oggfile) == -1) {
-        printf ("\nError: %s\n", vcedit_error (vc));
+    /* Move the original to a temposrary file */
+    sprintf (tmpfile, "%s.tmp", file);
+    rename (file, tmpfile);
+    
+    outfile = fopen (file, "wb");
+    if (!outfile)
+    {
+        printf ("\nError: Couldn't open '%s' for writing\n", file);
         exit (1);
     }
 
-    /* Remove Adonthell related comments, else we might get duplicates */
-    for (i = 0; i < vc->vc->comments; i++)
-        if (strncmp (vc->vc->user_comments[i], "OldHeaderSize", 13) == 0 ||
-            strncmp (vc->vc->user_comments[i], "StartPagePCM", 12) == 0 ||
-            strncmp (vc->vc->user_comments[i], "StartPageRaw", 12) == 0 ||
-            strncmp (vc->vc->user_comments[i], "Start", 5) == 0 ||
-            strncmp (vc->vc->user_comments[i], "End", 3) == 0)
-            {
-                int j;
-                free (vc->vc->user_comments[i]);
-                
-                for (j = i; j < vc->vc->comments - 1; j++)
-                {
-                    vc->vc->user_comments[j] = vc->vc->user_comments[j+1];
-                    vc->vc->comment_lengths[j] = vc->vc->comment_lengths[j+1];
-                }
-
-                changed = 1;
-                vc->vc->comments--;
-                i--;
-            }
-
-    if (changed) {
-        /* rename the input file */
-        sprintf (file, "%s.tmp", argv[argc-1]);
-        rename (argv[argc-1], file);        
-        outfile = fopen (argv[argc-1], "wb");
-    
-        /* write the changed file */
-        if (vcedit_write (vc, outfile) == -1) {
-            printf ("\nError: %s\n", vcedit_error (vc));
-
-            /* cleanup */
-            fclose (outfile);
-            unlink (argv[argc-1]);
-            rename (file, argv[argc-1]);
-            exit (1);
-        }
-
-        unlink (file);
-        
-        fclose (outfile);
-        fclose (oggfile);
-
-        /* reopen clean file */
-        oggfile = fopen (argv[argc-1], "rb");
-
-        if (vcedit_open (vc, oggfile) == -1) {
-            printf ("\nError: %s\n", vcedit_error (vc));
-            exit (1);
-        }
-    }
-
-    /* get the old file size */
-    i = ftell (oggfile);
-    fseek (oggfile, 0, SEEK_END);
-    size = ftell (oggfile);
-    fseek (oggfile, i, SEEK_SET);
-    
-    /* add the values */
-    sprintf (tmp, "%i", size);
-    vorbis_comment_add_tag (vc->vc, "OldHeaderSize", tmp);
-    sprintf (tmp, "%i", p_pcm);
-    vorbis_comment_add_tag (vc->vc, "StartPagePCM", tmp);
-    sprintf (tmp, "%i", p_raw);
-    vorbis_comment_add_tag (vc->vc, "StartPageRaw", tmp);
-    sprintf (tmp, "%i", s);
-    vorbis_comment_add_tag (vc->vc, "Start", tmp);
-    sprintf (tmp, "%i", e);
-    vorbis_comment_add_tag (vc->vc, "End", tmp);
-    
-    /* rename the input file */
-    sprintf (file, "%s.tmp", argv[argc-1]);
-    rename (argv[argc-1], file);        
-    outfile = fopen (argv[argc-1], "wb");
-    
     /* write the changed file */
-    if (vcedit_write (vc, outfile) == -1) {
-        printf ("\nError: %s\n", vcedit_error (vc));
+    if (vcedit_write (state, outfile) == -1)
+    {
+        printf ("\nError: %s\n", vcedit_error (state));
 
         /* cleanup */
         fclose (outfile);
-        unlink (argv[argc-1]);
-        rename (file, argv[argc-1]);
+        unlink (file);
+        rename (tmpfile, file);
+        vcedit_clear (state);
+        free (state);
         exit (1);
     }
 
-    unlink (file);
-    vcedit_clear (vc);
+    /* Everything went well -> delete the temporary file */
+    unlink (tmpfile);
 
     fclose (outfile);
-    fclose (oggfile);
-    
-  } else {
-    printf ("Input was not seekable.\n");
-    ov_clear (&ov);
-  }
+}
 
-  return 0;
+/* Remove any existing Adonthell related comments */
+void remove_comments (char *file)
+{
+    vcedit_state *state;
+    vorbis_comment *vc;
+    char changed = 0;
+    FILE *oggfile;
+    int i;
+
+    /* open the tune for vcedit */
+    oggfile = fopen (file, "rb");
+    if (!oggfile)
+    {
+        printf ("\nError: Couldn't open '%s' for reading\n", file);
+        exit (1);
+    }
+
+    /* add stuff to the oggvorbis comment header */
+    state = vcedit_new_state ();
+    if (vcedit_open (state, oggfile) == -1)
+    {
+        printf ("\nError: %s\n", vcedit_error (state));
+        exit (1);
+    }
+
+    vc = vcedit_comments (state);
+    
+    /* Remove Adonthell related comments, else we might get duplicates */
+    for (i = 0; i < vc->comments; i++)
+        if (strncmp (vc->user_comments[i], "OldHeaderSize", 13) == 0 ||
+            strncmp (vc->user_comments[i], "StartPagePCM", 12) == 0 ||
+            strncmp (vc->user_comments[i], "StartPageRaw", 12) == 0 ||
+            strncmp (vc->user_comments[i], "Start", 5) == 0 ||
+            strncmp (vc->user_comments[i], "End", 3) == 0)
+            {
+                int j;
+
+                free (vc->user_comments[i]);
+
+                for (j = i; j < vc->comments - 1; j++)
+                {
+                    vc->user_comments[j] = vc->user_comments[j + 1];
+                    vc->comment_lengths[j] = vc->comment_lengths[j + 1];
+                }
+
+                changed = 1;
+                vc->comments--;
+                i--;
+            }
+
+    if (changed) write_file (file, state);
+
+    /* Cleanup */
+    fclose (oggfile);
+    vcedit_clear (state);
+    free (state);
+}
+
+/* Add the newly calculated looping info as comments to the Vorbis File */
+void add_comments (char *file, int p_pcm, int p_raw, int s, int e)
+{
+    vcedit_state *state;
+    FILE *oggfile;
+    char tmp[32];
+    int size;
+
+    /* open the tune for vcedit */
+    oggfile = fopen (file, "rb");
+    if (!oggfile)
+    {
+        printf ("\nError: Couldn't open '%s' for reading\n", file);
+        exit (1);
+    }
+
+    /* get the file size before we add our comments */
+    fseek (oggfile, 0, SEEK_END);
+    size = ftell (oggfile);
+    fseek (oggfile, 0, SEEK_SET);
+
+    /* add stuff to the oggvorbis comment header */
+    state = vcedit_new_state ();
+    if (vcedit_open (state, oggfile) == -1)
+    {
+        printf ("\nError: %s\n", vcedit_error (state));
+        exit (1);
+    }
+
+    /* add the values */
+    sprintf (tmp, "%i", size);
+    vorbis_comment_add_tag (state->vc, "OldHeaderSize", tmp);
+    sprintf (tmp, "%i", p_pcm);
+    vorbis_comment_add_tag (state->vc, "StartPagePCM", tmp);
+    sprintf (tmp, "%i", p_raw);
+    vorbis_comment_add_tag (state->vc, "StartPageRaw", tmp);
+    sprintf (tmp, "%i", s);
+    vorbis_comment_add_tag (state->vc, "Start", tmp);
+    sprintf (tmp, "%i", e);
+    vorbis_comment_add_tag (state->vc, "End", tmp);
+
+    /* Save the new comments */
+    write_file (file, state);
+
+    /* Cleanup */
+    fclose (oggfile);
+    vcedit_clear (state);
+    free (state);
+}
+
+/* Calculate everything we need to find the start and end of our loop */
+void calculate_loop (int argc, char* argv[])
+{
+    OggVorbis_File ov;
+    FILE *oggfile;
+    int i, s, e, p_raw, p_pcm;
+    double start = 0;
+    double end = 0;
+
+    /* open the file, our last argument */
+    oggfile = fopen (argv[argc - 1], "r");
+
+    if (!oggfile)
+    {
+        printf ("File \"%s\" not found.\n\n", argv[argc - 1]);
+        exit (1);
+    }
+
+    if (ov_open (oggfile, &ov, NULL, -1) < 0)
+    {
+        printf ("Could not open input as an OggVorbis file.\n\n");
+        exit (1);
+    }
+
+    /* try to get start and end time */
+    for (i = 1; i < argc - 1; i++)
+    {
+        if (!strcmp ("-s", argv[i]) && i + 1 < argc)
+            start = atof (argv[++i]);
+        if (!strcmp ("-e", argv[i]) && i + 1 < argc)
+            end = atof (argv[++i]);
+    }
+
+    /* print details about each logical bitstream in the input */
+    if (ov_seekable (&ov))
+    {
+        if (!end)
+            end = ov_time_total (&ov, -1);
+
+        /* We want to loop a tune in ogg format the following way:
+
+           start           end
+           |------- + ------------ + -------|
+           intro        loop        outro
+
+           Get the starting point as pcm offset, since that makes the seeking
+           most precise */
+
+        ov_time_seek (&ov, start);
+        s = ov_pcm_tell (&ov);
+        printf ("\nStart (%gs):\t %i (pcm offset)\n", start, s);
+
+        /* To allow fast seeking, we'll get the start of the page containing
+           the pcm sample above, we then position the stream manually to the
+           page and skip data until we've found our pcm offset. That's nearly
+           as fast as ov_seek_raw () would be! */
+        ov_time_seek_page (&ov, start);
+        p_pcm = ov_pcm_tell (&ov);
+        p_raw = ov_raw_tell (&ov) - ov.oy.returned;
+
+        /* Get the end point as raw bytes */
+        ov_time_seek (&ov, end);
+        e = ov_raw_tell (&ov);
+        printf ("End (%gs):\t %i (raw bytes)\n", end, e);
+        fflush (stdout);
+
+        /* now add the comments to the vorbis file */
+        add_comments (argv[argc-1], p_pcm, p_raw, s, e);
+    }
+    else
+    {
+        printf ("Input was not seekable.\n");
+    }
+
+    /* close the file */
+    ov_clear (&ov);
+}
+
+/* oggloop */
+int main (int argc, char *argv[])
+{
+    if (argc <= 1 || argc > 6)
+    {
+        printf ("\nUsage: %s [-s <start>] [-e <end>] <OggVorbis file>\n\n", argv[0]);
+        exit (1);
+    } 
+
+    /* First remove any existing Adonthell related comments */
+    remove_comments (argv[argc-1]);
+
+    /* Then calculate the values we need and add them to the OggVorbis file */
+    calculate_loop (argc, argv);
+
+    return 0;
 }
