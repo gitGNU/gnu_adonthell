cace91733d4167ae00e87274f149e63f833bac71
Joel Vennin <jol@lunar-linux.org>
2002-02-20 14:52:39 +0000

    Added possibility to have callback with 1,2,3 arg and return a value

--- src/callback.h
+++ src/callback.h
@@ -54,6 +54,7 @@ class FunctorBase
 };
 
 
+/* No Argument, No return */
 class Functor0:public FunctorBase
 {
  private:
@@ -117,7 +118,10 @@ makeFunctor(TRT (*f)())
 }
 
 
-//NO ARG WITH RETURN
+
+
+
+/* Arg, with return */
 
 template <class RT>
 class Functor0wRet:public FunctorBase{
@@ -188,7 +192,10 @@ makeFunctor(Functor0wRet<RT>*,TRT (*f)())
   return FunctionTranslator0wRet<RT,TRT (*)()>(f);
 }
 
-// 1 Argument, no return value
+
+
+
+/* 1 Argument, no return value */
 template <class P1> class Functor1 : public FunctorBase
 {
 public:
@@ -282,6 +289,341 @@ makeFunctor (TRT (CallType::*const &f) () const)
     return MemberOf1stArgTranslator1 <P1, MemFunc> (f);
 }
 
+
+
+
+/* one arg, with return */
+template <class P1,class RT>
+class Functor1wRet:public FunctorBase{
+public:
+	Functor1wRet(){}
+	RT operator()(P1 p1)const
+		{
+		return (thunk(*this,p1));
+		}
+	//for STL
+	typedef P1 argument_type;
+	typedef RT result_type;
+protected:
+	typedef RT (*Thunk)(const FunctorBase &,P1);
+	Functor1wRet(Thunk t,const void *c,PFunc f,const void *mf,size_t sz):
+		FunctorBase(c,f,mf,sz),thunk(t){}
+private:
+	Thunk thunk;
+};
+
+template <class P1,class RT,class Callee, class MemFunc>
+class MemberTranslator1wRet:public Functor1wRet<P1,RT>{
+public:
+	MemberTranslator1wRet(Callee &c,const MemFunc &m):
+		Functor1wRet<P1,RT>(thunk,&c,0,&m,sizeof(MemFunc)){}
+	static RT thunk(const FunctorBase &ftor,P1 p1)
+		{
+		Callee *callee = (Callee *)ftor.getCallee();
+		MemFunc &memFunc 
+			(*(MemFunc*)(void *)(ftor.getMemFunc()));
+		return ((callee->*memFunc)(p1));
+		}
+};
+
+template <class P1,class RT,class Func>
+class FunctionTranslator1wRet:public Functor1wRet<P1,RT>{
+public:
+	FunctionTranslator1wRet(Func f):
+		Functor1wRet<P1,RT>(thunk,0,(PFunc)f,0,0){}
+	static RT thunk(const FunctorBase &ftor,P1 p1)
+		{
+		return (Func(ftor.getFunc()))(p1);
+		}
+};
+
+
+template <class P1,class RT,
+	class Callee,class TRT,class CallType,class TP1>
+inline MemberTranslator1wRet<P1,RT,Callee,TRT (CallType::*)(TP1)>
+makeFunctor(Functor1wRet<P1,RT>*,Callee &c,
+	TRT (CallType::*  f)(TP1))
+	{
+	typedef TRT (CallType::*MemFunc)(TP1);
+	return MemberTranslator1wRet<P1,RT,Callee,MemFunc>(c,f);
+	}
+
+
+template <class P1,class RT,
+	class Callee,class TRT,class CallType,class TP1>
+inline MemberTranslator1wRet<P1,RT,
+	const Callee,TRT (CallType::*)(TP1)const>
+makeFunctor(Functor1wRet<P1,RT>*,
+	const Callee &c,TRT (CallType::*  f)(TP1)const)
+	{
+	typedef TRT (CallType::*MemFunc)(TP1)const;
+	return MemberTranslator1wRet<P1,RT,const Callee,MemFunc>(c,f);
+	}
+
+template <class P1,class RT,class TRT,class TP1>
+inline FunctionTranslator1wRet<P1,RT,TRT (*)(TP1)>
+makeFunctor(Functor1wRet<P1,RT>*,TRT (*f)(TP1))
+	{
+	return FunctionTranslator1wRet<P1,RT,TRT (*)(TP1)>(f);
+	}
+
+template <class P1,class RT,class MemFunc>
+class MemberOf1stArgTranslator1wRet:public Functor1wRet<P1,RT>{
+public:
+	MemberOf1stArgTranslator1wRet(const MemFunc &m):
+		Functor1wRet<P1,RT>(thunk,(void *)1,0,&m,sizeof(MemFunc)){}
+	static RT thunk(const FunctorBase &ftor,P1 p1)
+		{
+		MemFunc &memFunc 
+			(*(MemFunc*)(void *)(ftor.getMemFunc()));
+		return ((p1.*memFunc)());
+		}
+};
+
+
+template <class P1,class RT,class TRT,class CallType>
+inline MemberOf1stArgTranslator1wRet<P1,RT,TRT (CallType::*)()>
+makeFunctor(Functor1wRet<P1,RT>*,TRT (CallType::*  f)())
+	{
+	typedef TRT (CallType::*MemFunc)();
+	return MemberOf1stArgTranslator1wRet<P1,RT,MemFunc>(f);
+	}
+
+
+template <class P1,class RT,class TRT,class CallType>
+inline MemberOf1stArgTranslator1wRet<P1,RT,TRT (CallType::*)()const>
+makeFunctor(Functor1wRet<P1,RT>*,TRT (CallType::*  f)()const)
+	{
+	typedef TRT (CallType::*MemFunc)()const;
+	return MemberOf1stArgTranslator1wRet<P1,RT,MemFunc>(f);
+	}
+
+
+
+/* two args, with return */
+template <class P1,class P2,class RT>
+class Functor2wRet:public FunctorBase{
+public:
+	Functor2wRet(){}
+	RT operator()(P1 p1,P2 p2)const
+		{
+		return (thunk(*this,p1,p2));
+		}
+	//for STL
+	typedef P1 first_argument_type;
+	typedef P2 second_argument_type;
+	typedef RT result_type;
+protected:
+	typedef RT (*Thunk)(const FunctorBase &,P1,P2);
+	Functor2wRet(Thunk t,const void *c,PFunc f,const void *mf,size_t sz):
+		FunctorBase(c,f,mf,sz),thunk(t){}
+private:
+	Thunk thunk;
+};
+
+template <class P1,class P2,class RT,class Callee, class MemFunc>
+class MemberTranslator2wRet:public Functor2wRet<P1,P2,RT>{
+public:
+	MemberTranslator2wRet(Callee &c,const MemFunc &m):
+		Functor2wRet<P1,P2,RT>(thunk,&c,0,&m,sizeof(MemFunc)){}
+	static RT thunk(const FunctorBase &ftor,P1 p1,P2 p2)
+		{
+		Callee *callee = (Callee *)ftor.getCallee();
+		MemFunc &memFunc 
+			(*(MemFunc*)(void *)(ftor.getMemFunc()));
+		return ((callee->*memFunc)(p1,p2));
+		}
+};
+
+template <class P1,class P2,class RT,class Func>
+class FunctionTranslator2wRet:public Functor2wRet<P1,P2,RT>{
+public:
+	FunctionTranslator2wRet(Func f):
+		Functor2wRet<P1,P2,RT>(thunk,0,(PFunc)f,0,0){}
+	static RT thunk(const FunctorBase &ftor,P1 p1,P2 p2)
+		{
+		return (Func(ftor.getFunc()))(p1,p2);
+		}
+};
+
+
+template <class P1,class P2,class RT,class Callee,
+	class TRT,class CallType,class TP1,class TP2>
+inline MemberTranslator2wRet<P1,P2,RT,Callee,
+	TRT (CallType::*)(TP1,TP2)>
+makeFunctor(Functor2wRet<P1,P2,RT>*,Callee &c,
+	TRT (CallType::*  f)(TP1,TP2))
+	{
+	typedef TRT (CallType::*MemFunc)(TP1,TP2);
+	return MemberTranslator2wRet<P1,P2,RT,Callee,MemFunc>(c,f);
+	}
+
+
+template <class P1,class P2,class RT,class Callee,
+	class TRT,class CallType,class TP1,class TP2>
+inline MemberTranslator2wRet<P1,P2,RT,const Callee,
+	TRT (CallType::*)(TP1,TP2)const>
+makeFunctor(Functor2wRet<P1,P2,RT>*,const Callee &c,
+	TRT (CallType::*  f)(TP1,TP2)const)
+	{
+	typedef TRT (CallType::*MemFunc)(TP1,TP2)const;
+	return MemberTranslator2wRet<P1,P2,RT,const Callee,MemFunc>(c,f);
+	}
+
+template <class P1,class P2,class RT,class TRT,class TP1,class TP2>
+inline FunctionTranslator2wRet<P1,P2,RT,TRT (*)(TP1,TP2)>
+makeFunctor(Functor2wRet<P1,P2,RT>*,TRT (*f)(TP1,TP2))
+	{
+	return FunctionTranslator2wRet<P1,P2,RT,TRT (*)(TP1,TP2)>(f);
+	}
+
+template <class P1,class P2,class RT,class MemFunc>
+class MemberOf1stArgTranslator2wRet:public Functor2wRet<P1,P2,RT>{
+public:
+	MemberOf1stArgTranslator2wRet(const MemFunc &m):
+		Functor2wRet<P1,P2,RT>(thunk,(void *)1,0,&m,sizeof(MemFunc)){}
+	static RT thunk(const FunctorBase &ftor,P1 p1,P2 p2)
+		{
+		MemFunc &memFunc 
+			(*(MemFunc*)(void *)(ftor.getMemFunc()));
+		return ((p1.*memFunc)(p2));
+		}
+};
+
+
+
+template <class P1,class P2,class RT,class TRT,class CallType,class TP1>
+inline MemberOf1stArgTranslator2wRet<P1,P2,RT,TRT (CallType::*)(TP1)>
+makeFunctor(Functor2wRet<P1,P2,RT>*,TRT (CallType::* f)(TP1))
+	{
+	typedef TRT (CallType::*MemFunc)(TP1);
+	return MemberOf1stArgTranslator2wRet<P1,P2,RT,MemFunc>(f);
+	}
+
+
+template <class P1,class P2,class RT,class TRT,class CallType,class TP1>
+inline MemberOf1stArgTranslator2wRet<P1,P2,RT,TRT (CallType::*)(TP1)const>
+makeFunctor(Functor2wRet<P1,P2,RT>*,
+	TRT (CallType::* f)(TP1)const)
+	{
+	typedef TRT (CallType::*MemFunc)(TP1)const;
+	return MemberOf1stArgTranslator2wRet<P1,P2,RT,MemFunc>(f);
+	}
+
+
+
+
+/* three args, with return */
+template <class P1,class P2,class P3,class RT>
+class Functor3wRet:public FunctorBase{
+public:
+	Functor3wRet(){}
+	RT operator()(P1 p1,P2 p2,P3 p3)const
+		{
+		return (thunk(*this,p1,p2,p3));
+		}
+protected:
+	typedef RT (*Thunk)(const FunctorBase &,P1,P2,P3);
+	Functor3wRet(Thunk t,const void *c,PFunc f,const void *mf,size_t sz):
+		FunctorBase(c,f,mf,sz),thunk(t){}
+private:
+	Thunk thunk;
+};
+
+template <class P1,class P2,class P3,
+	class RT,class Callee, class MemFunc>
+class MemberTranslator3wRet:public Functor3wRet<P1,P2,P3,RT>{
+public:
+	MemberTranslator3wRet(Callee &c,const MemFunc &m):
+		Functor3wRet<P1,P2,P3,RT>(thunk,&c,0,&m,sizeof(MemFunc)){}
+	static RT thunk(const FunctorBase &ftor,P1 p1,P2 p2,P3 p3)
+		{
+		Callee *callee = (Callee *)ftor.getCallee();
+		MemFunc &memFunc
+			(*(MemFunc*)(void *)(ftor.getMemFunc()));
+		return ((callee->*memFunc)(p1,p2,p3));
+		}
+};
+
+template <class P1,class P2,class P3,class RT,class Func>
+class FunctionTranslator3wRet:public Functor3wRet<P1,P2,P3,RT>{
+public:
+	FunctionTranslator3wRet(Func f):
+		Functor3wRet<P1,P2,P3,RT>(thunk,0,(PFunc)f,0,0){}
+	static RT thunk(const FunctorBase &ftor,P1 p1,P2 p2,P3 p3)
+		{
+		return (Func(ftor.getFunc()))(p1,p2,p3);
+		}
+};
+
+
+
+template <class P1,class P2,class P3,class RT,class Callee,
+	class TRT,class CallType,class TP1,class TP2,class TP3>
+inline MemberTranslator3wRet<P1,P2,P3,RT,Callee,
+	TRT (CallType::*)(TP1,TP2,TP3)>
+makeFunctor(Functor3wRet<P1,P2,P3,RT>*,Callee &c,
+	TRT (CallType::* f)(TP1,TP2,TP3))
+	{
+	typedef TRT (CallType::*MemFunc)(TP1,TP2,TP3);
+	return MemberTranslator3wRet<P1,P2,P3,RT,Callee,MemFunc>(c,f);
+	}
+
+
+template <class P1,class P2,class P3,class RT,class Callee,
+	class TRT,class CallType,class TP1,class TP2,class TP3>
+inline MemberTranslator3wRet<P1,P2,P3,RT,const Callee,
+	TRT (CallType::*)(TP1,TP2,TP3)const>
+makeFunctor(Functor3wRet<P1,P2,P3,RT>*,const Callee &c,
+	TRT (CallType::* f)(TP1,TP2,TP3)const)
+	{
+	typedef TRT (CallType::*MemFunc)(TP1,TP2,TP3)const;
+	return MemberTranslator3wRet<P1,P2,P3,RT,const Callee,MemFunc>(c,f);
+	}
+
+template <class P1,class P2,class P3,class RT,
+	class TRT,class TP1,class TP2,class TP3>
+inline FunctionTranslator3wRet<P1,P2,P3,RT,TRT (*)(TP1,TP2,TP3)>
+makeFunctor(Functor3wRet<P1,P2,P3,RT>*,TRT (*f)(TP1,TP2,TP3))
+	{
+	return FunctionTranslator3wRet<P1,P2,P3,RT,TRT (*)(TP1,TP2,TP3)>(f);
+	}
+
+template <class P1,class P2,class P3,class RT,class MemFunc>
+class MemberOf1stArgTranslator3wRet:public Functor3wRet<P1,P2,P3,RT>{
+public:
+	MemberOf1stArgTranslator3wRet(const MemFunc &m):
+		Functor3wRet<P1,P2,P3,RT>(thunk,(void *)1,0,&m,sizeof(MemFunc)){}
+	static RT thunk(const FunctorBase &ftor,P1 p1,P2 p2,P3 p3)
+		{
+		MemFunc &memFunc
+			(*(MemFunc*)(void *)(ftor.getMemFunc()));
+		return ((p1.*memFunc)(p2,p3));
+		}
+};
+
+
+template <class P1,class P2,class P3,class RT,class TRT,class CallType,
+	class TP1,class TP2>
+inline MemberOf1stArgTranslator3wRet<P1,P2,P3,RT,TRT (CallType::*)(TP1,TP2)>
+makeFunctor(Functor3wRet<P1,P2,P3,RT>*,
+	TRT (CallType::* f)(TP1,TP2))
+	{
+	typedef TRT (CallType::*MemFunc)(TP1,TP2);
+	return MemberOf1stArgTranslator3wRet<P1,P2,P3,RT,MemFunc>(f);
+	}
+
+
+template <class P1,class P2,class P3,class RT,class TRT,class CallType,
+	class TP1,class TP2>
+inline MemberOf1stArgTranslator3wRet<P1,P2,P3,RT,
+	TRT (CallType::*)(TP1,TP2)const>
+makeFunctor(Functor3wRet<P1,P2,P3,RT>*,
+	TRT (CallType::* f)(TP1,TP2)const)
+	{
+	typedef TRT (CallType::*MemFunc)(TP1,TP2)const;
+	return MemberOf1stArgTranslator3wRet<P1,P2,P3,RT,MemFunc>(f);
+	}
 
 #endif
 
