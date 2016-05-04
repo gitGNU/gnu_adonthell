41a4da176fd3dea55bde8c4952dbab54af608b55
Kai Sterker <kai.sterker@gmail.com>
2016-05-04 20:25:48 +0000

    UPDATED gettext to version 0.19.7 REMOVED included libintl

--- autogen.sh
+++ autogen.sh
@@ -3,16 +3,9 @@
 rm -f configure config.log config.cache
 rm -f config.status aclocal.m4
 
-# intl/Makefile.in is not generated, so don't delete it
-mv intl/Makefile.in intl/Makefile.bak 	 
-rm -f `find . -name 'Makefile.in'`
-mv intl/Makefile.bak intl/Makefile.in
-rm -f `find . -name 'Makefile'`
-
 # Regenerate everything
 
 aclocal -I m4
-libtoolize --automake --force --copy
 autoheader
 automake --add-missing --gnu --copy
 autoconf 
